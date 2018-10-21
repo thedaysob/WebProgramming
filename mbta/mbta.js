263ddc92a413366190d6de3e3351ac71e48d80b2
Daeseob Lim <thedaysob@gmail.com>
2018-10-21 16:14:59 -0400

    Station data is hardcoded

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -5,4 +5,97 @@ function initMap() {
         center: {lat: 42.352271, lng: -71.05524200000001},
         zoom: 13
     });
+
+/* MAP DATA */
+    var stations = [
+        {
+            position: new google.maps.LatLng(42.395428, -71.142483),
+            stop_id: 'alfcl',
+            stop_name: 'Alewife'
+        }, {
+            position: new google.maps.LatLng(42.39674, -71.121815),
+            stop_id: 'davis',
+            stop_name: 'Davis'
+        }, {
+            position: new google.maps.LatLng(42.3884, -71.11914899999999),
+            stop_id: 'portr',
+            stop_name: 'Porter Square'
+        }, {
+            position: new google.maps.LatLng(42.373362, -71.118956),
+            stop_id: 'harsq',
+            stop_name: 'Harvard Square'
+        }, {
+            position: new google.maps.LatLng(42.365486, -71.103802),
+            stop_id: 'cntsq',
+            stop_name: 'Central Square'
+        }, {
+            position: new google.maps.LatLng(42.36249079, -71.08617653),
+            stop_id: 'knncl',
+            stop_name: 'Kendall/MIT'
+        }, {
+            position: new google.maps.LatLng(42.361166, -71.070628),
+            stop_id: 'chmnl',
+            stop_name: 'Charles/MGH'
+        }, {
+            position: new google.maps.LatLng(42.35639457, -71.0624242),
+            stop_id: 'pktrm',
+            stop_name: 'Park Street'
+        }, {
+            position: new google.maps.LatLng(42.355518, -71.060225),
+            stop_id: 'dwnxg',
+            stop_name: 'Downtown Crossing'
+        }, {
+            position: new google.maps.LatLng(42.352271, -71.05524200000001),
+            stop_id: 'sstat',
+            stop_name: 'South Station'
+        }, {
+            position: new google.maps.LatLng(42.342622, -71.056967),
+            stop_id: 'brdwy',
+            stop_name: 'Broadway'
+        }, {
+            position: new google.maps.LatLng(42.330154, -71.057655),
+            stop_id: 'andrw',
+            stop_name: 'Andrew'
+        }, {
+            position: new google.maps.LatLng(42.320685, -71.052391),
+            stop_id: 'jfk',
+            stop_name: 'JFK/UMass'
+        }, {
+            position: new google.maps.LatLng(42.31129, -71.053331),
+            stop_id: 'shmnl',
+            stop_name: 'Savin Hill'
+        }, {
+            position: new google.maps.LatLng(42.300093, -71.061667),
+            stop_id: 'fldcr',
+            stop_name: 'Fields Corner'
+        }, {
+            position: new google.maps.LatLng(42.29312583, -71.06573796000001),
+            stop_id: 'smmnl',
+            stop_name: 'Shawmut'
+        }, {
+            position: new google.maps.LatLng(42.284652, -71.06448899999999),
+            stop_id: 'asmnl',
+            stop_name: 'Ashmont'
+        }, {
+            position: new google.maps.LatLng(42.275275, -71.029583),
+            stop_id: 'nqncy',
+            stop_name: 'North Quincy'
+        }, {
+            position: new google.maps.LatLng(42.2665139, -71.0203369),
+            stop_id: 'wlsta',
+            stop_name: 'Wollaston'
+        }, {
+            position: new google.maps.LatLng(42.251809, -71.005409),
+            stop_id: 'qnctr',
+            stop_name: 'Quincy Center'
+        }, {
+            position: new google.maps.LatLng(42.233391, -71.007153),
+            stop_id: 'qamnl',
+            stop_name: 'Quincy Adams'
+        },  {
+            position: new google.maps.LatLng(42.2078543, -71.0011385),
+            stop_id: 'brntn',
+            stop_name: 'Braintree'
+        }
+    ];
 }
